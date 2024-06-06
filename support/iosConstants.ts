8d295f6beee43388b91522aed158442d6eff3602
Kevin Pugliese <63055465+kevpug@users.noreply.github.com>
2024-06-06 19:49:02 -0400

    README & other info

--- support/iosConstants.ts
+++ support/iosConstants.ts
@@ -14,8 +14,8 @@ export const GROUP_IDENTIFIER_TEMPLATE_REGEX = /{{GROUP_IDENTIFIER}}/gm;
 export const BUNDLE_SHORT_VERSION_TEMPLATE_REGEX = /{{BUNDLE_SHORT_VERSION}}/gm;
 export const BUNDLE_VERSION_TEMPLATE_REGEX = /{{BUNDLE_VERSION}}/gm;
 
-export const DEFAULT_BUNDLE_VERSION = '1';
-export const DEFAULT_BUNDLE_SHORT_VERSION = '1.0';
+export const DEFAULT_BUNDLE_VERSION = "1";
+export const DEFAULT_BUNDLE_SHORT_VERSION = "1.0";
 
 export const NSE_TARGET_NAME = "NotifeeNotificationServiceExtension";
 export const NSE_SOURCE_FILE = "NotificationService.m";
